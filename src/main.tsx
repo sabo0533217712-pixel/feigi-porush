a7b12ca4c37ff48efa0cd7f96ec736e6d19901b0
rachel sabo <sabo0533217712@gmail.com>
2026-05-19 14:04:03 +0300

    Add console logs for document element properties
    Log the document element's class name and primary color.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-document.documentElement.classList.add("dark");
-
+console.log(document.documentElement.className);
+console.log(getComputedStyle(document.documentElement).getPropertyValue('--primary'))
 createRoot(document.getElementById("root")!).render(<App />);
 
